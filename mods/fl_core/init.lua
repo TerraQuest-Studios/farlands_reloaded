4e8007a999b3e0dfbe6d526beab46227563f56c0
wsor <24964441+wsor4035@users.noreply.github.com>
2021-04-19 23:09:09 -0400

    add formspec 4 join check

--- mods/fl_core/init.lua
+++ mods/fl_core/init.lua
@@ -1,2 +1,10 @@
 -- add sounds and core stuff here
 minetest.log("info", "[fl_core]: core loaded")
+
+minetest.register_on_joinplayer(function(player)
+    local pinfo = minetest.get_player_information(player:get_player_name())
+    if pinfo.formspec_version < 4  then
+        minetest.kick_player(player:get_player_name(), "please use a formspec v4 client (5.4) client")
+    end
+end
+)
