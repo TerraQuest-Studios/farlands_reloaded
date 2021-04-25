106156cd8a630e2c7b144bfa2b9389edcd62ece3
wsor <24964441+wsor4035@users.noreply.github.com>
2021-04-25 19:12:12 -0400

    more saplings

--- mods/fl_trees/saplings.lua
+++ mods/fl_trees/saplings.lua
@@ -24,3 +24,28 @@ minetest.register_node("fl_trees:aspen_sapling", {
     selection_box = {type = "fixed", fixed = {-0.2, -0.5, -0.2, 0.2, 0.5, 0.2}},
     collision_box = {type = "fixed", fixed = {-0.2, -0.5, -0.2, 0.2, 0.5, 0.2}}
 })
+
+minetest.register_node("fl_trees:baobab_sapling", {
+    description = "baobab sapling",
+    drawtype = "plantlike",
+    paramtype = "light",
+    sunlight_propagates = true,
+    walkable = true,
+    tiles = {"farlands_baobab_sapling.png"},
+    groups = {oddly_breakable_by_hand = 3},
+    on_timer = function(pos)
+        minetest.remove_node(pos)
+        minetest.place_schematic({x=pos.x-3, y=pos.y, z=pos.z-3}, modpath .. "/schems/baobab_tree_" .. math.random(5) .. ".mts", 0, nil, false)
+    end,
+    on_costruct = function(pos)
+        minetest.remove_node(pos)
+        minetest.get_node_timer(pos):start(20)
+    end,
+    on_place = function(itemstack, placer, pointed_thing)
+        if pointed_thing.type ~= "node" then return end
+        minetest.get_node_timer(pointed_thing.above):start(20)
+        return minetest.item_place(itemstack, placer, pointed_thing)
+    end,
+    selection_box = {type = "fixed", fixed = {-0.2, -0.5, -0.2, 0.2, 0.5, 0.2}},
+    collision_box = {type = "fixed", fixed = {-0.2, -0.5, -0.2, 0.2, 0.5, 0.2}}
+})
