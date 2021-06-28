9a87fa3bc83106826d1b24098f8cf95ce0512c60
wsor <24964441+wsor4035@users.noreply.github.com>
2021-06-27 22:47:26 -0400

    update mapgen water settings

--- mods/fl_mapgen/aliases.lua
+++ mods/fl_mapgen/aliases.lua
@@ -1,3 +1,8 @@
 minetest.register_alias("mapgen_stone", "fl_stone:stone")
-minetest.register_alias("mapgen_water_source", "fl_liquids:water_source")
-minetest.register_alias("mapgen_river_water_source", "fl_liquids:river_water_source")
+if minetest.get_mapgen_setting("mg_name") == "valleys" then
+    minetest.register_alias("mapgen_water_source", "fl_liquids:river_water_source")
+    minetest.register_alias("mapgen_river_water_source", "fl_liquids:river_water_source")
+else
+    minetest.register_alias("mapgen_water_source", "fl_liquids:water_source")
+    minetest.register_alias("mapgen_river_water_source", "fl_liquids:water_source")
+end
