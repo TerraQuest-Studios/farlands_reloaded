3fa3f40ce6fb68d749630a0177b7ae168af796f1
unknown <24964441+wsor4035@users.noreply.github.com>
2021-11-26 21:19:57 -0500

    cleanup and axe functionality for mobs

--- mods/fl_wildlife/fl_mobs/villager.lua
+++ mods/fl_wildlife/fl_mobs/villager.lua
@@ -1,18 +1,93 @@
---[[
+local sstep = minetest.settings:get("dedicated_server_step") or 0.09
+local creative = minetest.settings:get_bool("creative_mode", false)
+local random = math.random
+
+local function actfunc(self, staticdata, dtime_s)
+    fl_wildlife.actfunc(self, staticdata, dtime_s)
+    self.axe = mobkit.recall(self, "axe")
+end
+
+local function axe_behavior(self,priority)
+    local function use_axe()
+        if not creative then
+            self.axe[2] = self.axe[2] - 1
+            if self.axe[2] == 1 then
+                local pos = self.object:get_pos()
+                local anvil = minetest.find_nodes_in_area(
+                    vector.new(pos.x+5, pos.y+5, pos.z+5),
+                    vector.new(pos.x-5, pos.y-5, pos.z-5),
+                    "fl_workshop:anvil"
+                )
+                if #anvil >= 1 then
+                    self.axe[2] = minetest.registered_items[self.axe[1]].tool_capabilities.groupcaps.dig_tree.uses
+                end
+            end
+        end
+    end
+
+    local nodes = {}
+    local init = true
+    local turn = 0
+    local function func()
+        if init then
+            local pos = self.object:get_pos()
+            nodes = minetest.find_nodes_in_area(
+                vector.new(pos.x+7, pos.y+5, pos.z+7),
+                vector.new(pos.x-7, pos.y-5, pos.z-7),
+                "group:trunk"
+            )
+            init = false
+
+            if #nodes == 0 then return true end
+        end
+
+        if turn <= 1 and self.axe[2] > 0 then
+            if not minetest.is_protected(nodes[#nodes], "") then
+                minetest.dig_node(nodes[#nodes])
+                use_axe()
+            end
+            nodes[#nodes] = nil
+        elseif self.axe[2] == 0 then
+            self.axe = nil
+            mobkit.forget(self, "axe")
+            return true
+        --[[
+        --punch animation
+        elseif turn > math.floor((1/sstep)*3)-1 then
+            mobkit.animate(self, "punch")
+            turn = turn - 1
+            return false
+        --]]
+        else
+            turn = turn - 1
+            return false
+        end
+
+        if #nodes == 0 then
+            if not creative then
+                mobkit.remember(self, "axe", self.axe)
+            else
+                mobkit.forget(self, "axe")
+                self.axe = nil
+            end
+            return true
+        end
+
+        mobkit.lq_turn2pos(self, nodes[#nodes])
+        turn = math.floor((1/sstep)*3) --roughly 30 seconds
+
+    end
+    mobkit.queue_high(self,func,priority)
+end
+
 local function villager_brain(self)
     if mobkit.timer(self,1) then fl_wildlife.node_dps_dmg(self) end --if in nodes with damage take damage
     mobkit.vitals(self)
 
     if self.hp <= 0 then --kill self if 0 hp
-        local item_drops = fl_wildlife.drops("fl_wildlife.villager.drops")
-        if item_drops ~= nil then
-            for _, i in pairs(item_drops) do
-                minetest.add_item(mobkit.get_stand_pos(self), i)
-            end
-        end
 
         mobkit.clear_queue_high(self)
-        mobkit.hq_die(self)
+        fl_wildlife.hq_die(self)
         return
     end
 
@@ -24,35 +99,38 @@ local function villager_brain(self)
             return
         end
 
+        if self.axe and random(100) > 95 then
+            axe_behavior(self, 30)
+        end
+
         if mobkit.is_queue_empty_high(self) then
-            --mobkit.animate(self, "walk")
             mobkit.hq_roam(self,0)
-            --fl_wildlife.hq_npc_roam(self, 0)
-            --fhq_roam(self, 1)
         end
 
     end
 
 end
---]]
 
 minetest.register_entity("fl_wildlife:villager", {
     --mte object properties
-    physical = true,
-    stepheight = 1.1,
-    collide_with_objects = true,
-    collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.7, 0.3}, --top y set to 1.99 to walk under 2 nodes
-    visual = "mesh",
-    mesh = "farlands_npc_villager.b3d",
-    textures = {"farlands_npc_villager1.png", "farlands_npc_villager2.png"},
-    visual_size = {x = 0.9, y = 0.9, z = 0.9},
-    static_save = true,
+    initial_properties = {
+        physical = true,
+        stepheight = 1.1,
+        collide_with_objects = true,
+        collisionbox = {-0.3, 0.0, -0.3, 0.3, 1.7, 0.3},
+        visual = "mesh",
+        mesh = "farlands_npc_villager.b3d",
+        textures = {"farlands_npc_villager1.png", "farlands_npc_villager2.png"},
+        visual_size = {x = 0.9, y = 0.9, z = 0.9},
+        static_save = true,
+        --damage_texture_modifier = "^[colorize:#FF000040"
+    },
 
     --hp_max = 200,
 
     --mte entity properties
     on_step = mobkit.stepfunc, --this is required
-    on_activate = fl_wildlife.actfunc, --this is required as well(useing custom that calls mobkits and adds nametags)
+    on_activate = actfunc, --this is required as well(useing custom that calls mobkits and adds nametags)
     get_staticdata = mobkit.statfunc, --who knows, no documentation (probably save entity data)
 
     --mobkit properties
@@ -74,10 +152,7 @@ minetest.register_entity("fl_wildlife:villager", {
         punch = {range = {x=45,y=65}, speed = 10, loop = true},
     },
 
-    --custom to this mod
-    drops = "fl_wildlife.villager.drops",
-
-    brainfunc = fl_brains.villager_brain,--villager_brain, --function for the brain
+    brainfunc = villager_brain,--villager_brain, --function for the brain
     _egg_it = {"villager", "#654321"},
 
     --more mte properties
@@ -94,6 +169,15 @@ minetest.register_entity("fl_wildlife:villager", {
 
     on_rightclick = function(self, clicker)
         fl_wildlife.rclick_name(self, clicker)
+
+        local item = clicker:get_wielded_item():get_name()
+        if minetest.get_item_group(item, "axe") then
+            self.axe = {
+                clicker:get_wielded_item():get_name(),
+                minetest.registered_items[item].tool_capabilities.groupcaps.dig_tree.uses
+            }
+            mobkit.remember(self, "axe", self.axe)
+        end
     end,
 })
 
--- mods/fl_wildlife/init.lua
+++ mods/fl_wildlife/init.lua
@@ -1,5 +1,4 @@
 fl_wildlife = {}
-fl_brains = {}
 
 fl_wildlife.details = {
     version = 11,
@@ -8,19 +7,12 @@ fl_wildlife.details = {
     license = "MIT",
 }
 
-fl_brains.details = fl_wildlife.details
-
 local modpath = minetest.get_modpath(minetest.get_current_modname())
 
 dofile(modpath .. "/api.lua")
 dofile(modpath .. "/node_items.lua")
 dofile(modpath .. "/crafts.lua")
 dofile(modpath .. "/commands.lua")
-
-local mob_brains = {
-    "villager_brain",
-    "fish_brain",
-}
 
 local mob_list = {
     "villager",
@@ -33,15 +25,8 @@ local mob_list = {
     "cavecrab",
 }
 
-for _, brain in pairs(mob_brains) do
-    dofile(modpath .. "/fl_brains/" .. brain .. ".lua")
-end
-
 for _, mob in pairs(mob_list) do
-    local setting = "fl_wildlife." .. mob .. ".enable"
-    if minetest.settings:get_bool(setting, true) == true then
-        dofile(modpath .. "/fl_mobs/" .. mob .. ".lua")
-    end
+    dofile(modpath .. "/fl_mobs/" .. mob .. ".lua")
 end
 
 dofile(modpath .. "/spawner.lua")
