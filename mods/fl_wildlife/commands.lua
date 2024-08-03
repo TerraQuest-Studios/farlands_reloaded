ec05ce55a2c2cfa3d485a017c7c81620454a3708
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2024-08-03 15:10:49 -0400

    make clearents command use kill command internally since it supports its use case

--- mods/fl_wildlife/commands.lua
+++ mods/fl_wildlife/commands.lua
@@ -1,25 +1,3 @@
-minetest.register_chatcommand("clearents", {
-    description = "remove sepcified ents",
-    params = "<ent_name><radius>",
-    privs = {server = true},
-    func = function(name, param)
-        local counter = 0
-        local player = minetest.get_player_by_name(name)
-        local split = param:split(" ")
-        local radius = 32
-        if split[2] then radius = tonumber(split[2]) end
-        if not minetest.registered_entities[split[1]] then return false, "not valid ent" end
-        local objects = minetest.get_objects_inside_radius(player:get_pos(), radius)
-
-        for _, obj in pairs(objects) do
-            local ent = obj:get_luaentity()
-            if ent and ent.name == split[1] then obj:remove() counter = counter+1 end
-        end
-
-        return true, "[fl_wildlife]: " .. counter .. " ents removed"
-    end
-})
-
 --override this core command to allow killing mobs
 minetest.override_chatcommand("kill", {
     params = "<playername|mobname> [<radius>]",
@@ -74,5 +52,18 @@ minetest.override_chatcommand("kill", {
         end
 
         return false, "Invalid entity name or player name"
+    end
+})
+
+minetest.register_chatcommand("clearents", {
+    description = "remove sepcified ents",
+    params = "<ent_name><radius>",
+    privs = {server = true},
+    func = function(name, param)
+        minetest.chat_send_player(
+            name,
+            minetest.colorize("red", "WARNING: this command is depreciated, use /kill in the future")
+        )
+        return minetest.registered_chatcommands.kill.func(name, param)
     end
 })
