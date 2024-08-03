fc018f2d3bd9e596e96a9413b16065fd54aa9040
wsor4035 <24964441+wsor4035@users.noreply.github.com>
2024-08-03 14:50:30 -0400

    override the kill command to allow killing mobs

--- mods/fl_wildlife/commands.lua
+++ mods/fl_wildlife/commands.lua
@@ -19,3 +19,44 @@ minetest.register_chatcommand("clearents", {
         return true, "[fl_wildlife]: " .. counter .. " ents removed"
     end
 })
+
+--override this core command to allow killing mobs
+minetest.override_chatcommand("kill", {
+    params = "<playername|mobname>",
+    description = "Kill entity(mob), player, or yourself",
+    func = function(name, param)
+        local player = minetest.get_player_by_name(param)
+
+        if player then
+            if minetest.settings:get_bool("enable_damage") == false then
+                return false, "You can not kill players as damage is disabled"
+            end
+
+            if player:get_hp() <= 0 then
+                return false, "Target player " .. param .. " is already dead"
+            end
+
+            player:set_hp(0)
+            return true, "Player " .. param .. " killed"
+        end
+
+        local entity = minetest.registered_entities[param]
+
+        if entity then
+            local removed_ent_count = 0
+
+            for _, ent in pairs(minetest.luaentities) do
+                if ent.name == param then
+                    --maybe consider adding a check to see if the entity is a mob
+                    --and setting its hp to 0 so it can run its death callback
+                    ent.object:remove()
+                    removed_ent_count = removed_ent_count + 1
+                end
+            end
+
+            return true, "Removed " .. removed_ent_count .. " entities of type " .. param
+        end
+
+        return false, "Invalid entity name or player name"
+    end
+})
